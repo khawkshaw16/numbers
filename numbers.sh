2da15aba660a5c9ad2fbe583c45961678108192c
Kelly Hawkshaw <hawkshaw@chapman.edu>
2018-11-16 12:01:12 -0800

    Adds numbers.sh

--- numbers.sh
+++ numbers.sh
@@ -1,7 +1,17 @@
 #! /bin/bash
 #numbers.sh
-#Kelly Hawkshaw
+# Kelly Hawkshaw
 
 echo "Enter a positive number: "
-read numbers
-while echo $numbers
+read number
+N=0
+while [ $N -lt $number ]
+do
+	N=$((N+1))
+	if [ $((N%2)) -eq 0 ]
+	then
+		echo "$N is even"
+	else
+		echo "$N is odd"
+	fi
+done
